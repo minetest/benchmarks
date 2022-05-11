785fc85deca80365b05343a4440c389119965394
paradust7 <102263465+paradust7@users.noreply.github.com>
2022-05-11 23:48:14 +0000

    Skip the build when there are no updates

--- push-revs.sh
+++ push-revs.sh
@@ -7,6 +7,12 @@ if ! git diff-index --quiet --cached HEAD ; then
   echo "Update detected. Pushing changes"
   git commit -m "Minetest: ${MINETEST_REV} | Irrlicht: ${IRRLICHT_REV}"
   git push "https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+  DID_PUSH=true
 else
   echo "No update detected"
+  DID_PUSH=false
+fi
+
+if [ -n "${GITHUB_ENV:-}" ]; then
+  echo "DID_PUSH=$DID_PUSH" >> "$GITHUB_ENV"
 fi
