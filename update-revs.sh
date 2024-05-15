398ccf3a99f39fc529eb4f969e840fc8070e2013
sfan5 <sfan5@live.de>
2024-05-15 22:23:56 +0200

    Stop tracking Irrlicht; newer OS version

--- update-revs.sh
+++ update-revs.sh
@@ -1,11 +1,9 @@
 #!/bin/bash -eux
 
 git ls-remote "https://github.com/${GITHUB_REPOSITORY_OWNER}/minetest" refs/heads/master | awk '{print $1}' > minetest.rev
-git ls-remote "https://github.com/${GITHUB_REPOSITORY_OWNER}/irrlicht" refs/heads/master | awk '{print $1}' > irrlicht.rev
 
-git add minetest.rev irrlicht.rev
+git add minetest.rev
 
 if [ -n "${GITHUB_ENV:-}" ]; then
   echo "MINETEST_REV=$(cat minetest.rev)" >> "$GITHUB_ENV"
-  echo "IRRLICHT_REV=$(cat irrlicht.rev)" >> "$GITHUB_ENV"
 fi
