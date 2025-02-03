13a462d88448b0870959b1f91c54a0668c199fb9
Lars Mueller <appgurulars@gmx.de>
2025-02-03 20:06:32 +0100

    Fix repo link in another place

--- update-revs.sh
+++ update-revs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -eux
 
-git ls-remote "https://github.com/${GITHUB_REPOSITORY_OWNER}/minetest" refs/heads/master | awk '{print $1}' > minetest.rev
+git ls-remote "https://github.com/${GITHUB_REPOSITORY_OWNER}/luanti" refs/heads/master | awk '{print $1}' > minetest.rev
 
 git add minetest.rev
 
